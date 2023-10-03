cc3166d4c60da5d001eac47fa2b10fcffe46b8c9
Viktor Vorobev <vikeyer@gmail.com>
2023-10-03 11:43:00 +0200

    src: serializers: add serializer for db connection settings

--- src/monitor/serializers/__init__.py
+++ src/monitor/serializers/__init__.py
@@ -1,8 +1,10 @@
 from .monitoring_result import MonitoringResult
-from .settings import MonitorSettings, WebsiteSetting
+from .settings import MonitorSettings, WebsiteSetting, DbConnectionSettings, DbWorkerSettings
 
 __all__ = [
     'WebsiteSetting',
     'MonitorSettings',
     'MonitoringResult',
+    'DbConnectionSettings',
+    'DbWorkerSettings',
 ]
--- src/monitor/serializers/settings.py
+++ src/monitor/serializers/settings.py
@@ -1,6 +1,7 @@
 from typing import List
 
 import pydantic
+import pydantic_settings
 
 
 class WebsiteSetting(pydantic.BaseModel):
@@ -9,5 +10,29 @@ class WebsiteSetting(pydantic.BaseModel):
     regexp: str = ''
 
 
+class DbWorkerSettings(pydantic.BaseModel):
+    period: float = pydantic.Field(ge=0, le=300)
+    max_batch_size: int
+
+
 class MonitorSettings(pydantic.BaseModel):
     websites: List[WebsiteSetting]
+    db: DbWorkerSettings
+
+
+class DbConnectionSettings(pydantic_settings.BaseSettings):
+    model_config = pydantic_settings.SettingsConfigDict(env_prefix='postgresql_')
+
+    host: str
+    port: str
+    username: str
+    password: str
+    db: str
+    ssl: bool = True
+
+    @property
+    def dsn(self) -> str:
+        dsn = f'postgres://{self.username}:{self.password}@{self.host}:{self.port}/{self.db}'
+        if self.ssl:
+            dsn += '?sslmode=require'
+        return dsn
